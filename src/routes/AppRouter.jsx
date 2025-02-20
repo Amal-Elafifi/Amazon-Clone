5c2d6279ae65c0c4c6f6d4c6a27ddf9bb3570d77
AhmedTahasubs <ahmedtahaamuhammed@gmail.com>
2025-02-20 18:40:39 +0200

    store login signup commit

--- src/routes/AppRouter.jsx
+++ src/routes/AppRouter.jsx
@@ -1,34 +1,40 @@
-import { lazy, Suspense } from 'react';
-import {BrowserRouter, Routes, Route} from "react-router-dom";
+import { lazy, Suspense } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 const Home = lazy(() => import("../pages/Home"));
 const Cart = lazy(() => import("../pages/Cart"));
 const Categories = lazy(() => import("../pages/Categories"));
-const Login = lazy(() => import("../pages/Login"));
+const Login = lazy(() => import("../authPages/Login"));
 const Products = lazy(() => import("../pages/Products"));
-const SignUp = lazy(() => import("../pages/SignUp"));
+const SignUp = lazy(() => import("../authPages/SignUp"));
 const Wishlist = lazy(() => import("../pages/Wishlist"));
 const Product = lazy(() => import("../components/ecommerce/product/Product"));
-const Category = lazy(() => import("../components/ecommerce/category/Category"));
-import Loading from '../components/feedback/Loading';
-
-
+const Category = lazy(() =>
+  import("../components/ecommerce/category/Category")
+);
+import Loading from "../components/feedback/Loading";
+import { Provider } from "react-redux";
+import { store } from "../store/store";
+import { Toaster } from "react-hot-toast";
 const AppRouter = () => {
-    return (
-        <BrowserRouter>
-            <Suspense fallback={<Loading/>}>
-                <Routes>
-                <Route path="/" index element={<Home/>}/>
-                <Route path="/signUp" element={<SignUp/>}/>
-                <Route path="/login" element={<Login/>} />
-                <Route path="/categories" element={<Categories/>} />
-                <Route path="/category/:id" element={<Category/>} />
-                <Route path="/products" element={<Products/>} />
-                <Route path="/product/:id" element={<Product/>} />
-                <Route path="/cart" element={<Cart/>} />
-                <Route path="/wishlist" element={<Wishlist/>} />
-                </Routes>
-            </Suspense>
-        </BrowserRouter>
-    )   
-}
+  return (
+    <Provider store={store}>
+      <Toaster />
+      <BrowserRouter>
+        <Suspense fallback={<Loading />}>
+          <Routes>
+            <Route path="/" index element={<Home />} />
+            <Route path="/signUp" element={<SignUp />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/categories" element={<Categories />} />
+            <Route path="/category/:id" element={<Category />} />
+            <Route path="/products" element={<Products />} />
+            <Route path="/product/:id" element={<Product />} />
+            <Route path="/cart" element={<Cart />} />
+            <Route path="/wishlist" element={<Wishlist />} />
+          </Routes>
+        </Suspense>
+      </BrowserRouter>
+    </Provider>
+  );
+};
 export default AppRouter;
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@
 export default {
   content: ["./src/**/*.{jsx,js,ts,tsx}","./index.html"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['Inter', 'ui-sans-serif', 'system-ui'],
+        serif: ['Alegreya Sans', 'ui-serif', 'Georgia'],
+        mono: ['IBM Plex Sans Condensed', 'ui-monospace', 'SFMono-Regular'],
+        inika: ["Inika", 'serif'], // Add el custom hna
+      },
+    },
   },
   plugins: [],
 }
