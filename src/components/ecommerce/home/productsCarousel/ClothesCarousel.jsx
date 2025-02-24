e99487ae60057936f4c16145db8ae82c2c0031d3
Amal-Elafifi <160831057+Amal-Elafifi@users.noreply.github.com>
2025-02-24 02:41:52 -1100

    home section part1

---
+++ src/components/ecommerce/home/productsCarousel/ClothesCarousel.jsx
@@ -0,0 +1,24 @@
+import ProductsCarousel from "./ProductsCarousel";
+
+
+function ClothesCarousel () {
+    return(
+        <ProductsCarousel 
+            img1={<img src="/public/assets/images/clothes/cc21.png"/>}
+            img2={<img src="/public/assets/images/clothes/cc6.png"/>}
+            img3={<img src="/public/assets/images/clothes/cc19.png"/>}
+            img4={<img src="/public/assets/images/clothes/cc8.png"/>}
+            img5={<img src="/public/assets/images/clothes/cc9.png"/>}
+            img6={<img src="/public/assets/images/clothes/cc10.png"/>}
+            img7={<img src="/public/assets/images/clothes/cc5.png"/>}
+            img8={<img src="/public/assets/images/clothes/cc12.png"/>}
+            img9={<img src="/public/assets/images/clothes/cc7.png"/>}
+            img10={<img src="/public/assets/images/clothes/cc14.png"/>}
+            img11={<img src="/public/assets/images/clothes/cc15.png"/>}
+            img12={<img src="/public/assets/images/clothes/cc17.png"/>}
+
+        />        
+    )
+}
+
+export default ClothesCarousel;
