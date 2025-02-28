500f583d6080263e23573d1993c65438d02a1d0a
AhmedTahasubs <ahmedtahaamuhammed@gmail.com>
2025-02-28 15:17:23 +0200

    trying something

--- src/components/ecommerce/home/Landing.jsx
+++ src/components/ecommerce/home/Landing.jsx
@@ -1,6 +1,8 @@
 import { useState } from 'react';
 import Carousel from 'react-bootstrap/Carousel';
-
+import video1 from '/public/assets/videos/1.mp4';
+import video2 from '/public/assets/videos/2.mp4';
+import video3 from '/public/assets/videos/3.mp4';
 function Landing () {
     const [index, setIndex] = useState(0);
     const handleSelect = (selectedIndex) => {
@@ -10,15 +12,15 @@ function Landing () {
         <div className="h-[200px]">
             <Carousel className='max-h-[600px] overflow-hidden'>
             <Carousel.Item>
-                <video src="/public/assets/videos/1.mp4" type="video/mp4" width="100%"  autoPlay muted loop onLoadedData={(e) => e.target.play()}  
+                <video src={video1} type="video/mp4" width="100%"  autoPlay muted loop onLoadedData={(e) => e.target.play()}  
                 />
             </Carousel.Item>
             <Carousel.Item>
-                <video src="/public/assets/videos/2.mp4" type="video/mp4" width="100%"  autoPlay muted loop onLoadedData={(e) => e.target.play()}  
+                <video src={video2} type="video/mp4" width="100%"  autoPlay muted loop onLoadedData={(e) => e.target.play()}  
                 />
             </Carousel.Item>
             <Carousel.Item>
-             <video src="/public/assets/videos/3.mp4" type="video/mp4" width="100%"  autoPlay muted loop onLoadedData={(e) => e.target.play()}  
+             <video src={video3} type="video/mp4" width="100%"  autoPlay muted loop onLoadedData={(e) => e.target.play()}  
              />
             </Carousel.Item>
         </Carousel>
