638c2947438ab6916b70f04115e6b13283f61468
AhmedTahasubs <ahmedtahaamuhammed@gmail.com>
2025-03-02 19:47:29 +0200

    last check

--- src/components/ecommerce/home/productsCarousel/ClothesCarousel.jsx
+++ src/components/ecommerce/home/productsCarousel/ClothesCarousel.jsx
@@ -1,21 +1,32 @@
 import ProductsCarousel from "./ProductsCarousel";
-
+import cc21 from "/public/assets/images/clothes/cc21.png";
+import cc6 from "/public/assets/images/clothes/cc6.png";
+import cc19 from "/public/assets/images/clothes/cc19.png";
+import cc8 from "/public/assets/images/clothes/cc8.png";
+import cc9 from "/public/assets/images/clothes/cc9.png";
+import cc10 from "/public/assets/images/clothes/cc10.png";
+import cc5 from "/public/assets/images/clothes/cc5.png";
+import cc12 from "/public/assets/images/clothes/cc12.png";
+import cc7 from "/public/assets/images/clothes/cc7.png";
+import cc14 from "/public/assets/images/clothes/cc14.png";
+import cc15 from "/public/assets/images/clothes/cc15.png";
+import cc17 from "/public/assets/images/clothes/cc17.png";
 
 function ClothesCarousel () {
     return(
         <ProductsCarousel 
-            img1={<img src="/public/assets/images/clothes/cc21.png"/>}
-            img2={<img src="/public/assets/images/clothes/cc6.png"/>}
-            img3={<img src="/public/assets/images/clothes/cc19.png"/>}
-            img4={<img src="/public/assets/images/clothes/cc8.png"/>}
-            img5={<img src="/public/assets/images/clothes/cc9.png"/>}
-            img6={<img src="/public/assets/images/clothes/cc10.png"/>}
-            img7={<img src="/public/assets/images/clothes/cc5.png"/>}
-            img8={<img src="/public/assets/images/clothes/cc12.png"/>}
-            img9={<img src="/public/assets/images/clothes/cc7.png"/>}
-            img10={<img src="/public/assets/images/clothes/cc14.png"/>}
-            img11={<img src="/public/assets/images/clothes/cc15.png"/>}
-            img12={<img src="/public/assets/images/clothes/cc17.png"/>}
+            img1={<img src={cc21}/>}
+            img2={<img src={cc6}/>}
+            img3={<img src={cc19}/>}
+            img4={<img src={cc8}/>}
+            img5={<img src={cc9}/>}
+            img6={<img src={cc10}/>}
+            img7={<img src={cc5}/>}
+            img8={<img src={cc12}/>}
+            img9={<img src={cc7}/>}
+            img10={<img src={cc14}/>}
+            img11={<img src={cc15}/>}
+            img12={<img src={cc17}/>}
 
         />        
     )
--- src/components/ecommerce/home/productsCarousel/ItemsCarousel.jsx
+++ src/components/ecommerce/home/productsCarousel/ItemsCarousel.jsx
@@ -1,21 +1,33 @@
 import ProductsCarousel from "./ProductsCarousel";
+import bottles from "/public/assets/images/items/bottles.png";
+import jars from "/public/assets/images/items/jars.png";
+import rack from "/public/assets/images/items/rack.png";
+import kettle from "/public/assets/images/items/kettle.png";
+import chopper1 from "/public/assets/images/items/chopper1.png";
+import airfryer from "/public/assets/images/items/airfryer.png";
+import blender from "/public/assets/images/items/blender.png";
+import machine from "/public/assets/images/items/machine.png";
+import mixer from "/public/assets/images/items/mixer.png";
+import chopper from "/public/assets/images/items/chopper.png";
+import processor from "/public/assets/images/items/processor.png";
+import org from "/public/assets/images/items/org.png";
 
 function ItemsCarousel () {
     return (
         <>
         <ProductsCarousel 
-            img1={<img src="/public/assets/images/items/bottles.png" className="max-w-[90%]"/>}
-            img2={<img src="/public/assets/images/items/rack.png" className="max-w-[90%]"/>}
-            img3={<img src="/public/assets/images/items/jars.png" className="max-w-[90%]"/>}
-            img4={<img src="/public/assets/images/items/kettle.png" className="max-w-[90%]"/>}
-            img5={<img src="/public/assets/images/items/chopper1.png" className="max-w-[90%]"/>}
-            img6={<img src="/public/assets/images/items/airfryer.png" className="max-w-[90%]"/>}
-            img7={<img src="/public/assets/images/items/blender.png" className="max-w-[90%]"/>}
-            img8={<img src="/public/assets/images/items/machine.png" className="max-w-[90%]"/>}
-            img9={<img src="/public/assets/images/items/mixer.png" className="max-w-[90%]"/>}
-            img10={<img src="/public/assets/images/items/chopper.png" className="max-w-[90%]"/>}
-            img11={<img src="/public/assets/images/items/processor.png" className="max-w-[90%]"/>}
-            img12={<img src="/public/assets/images/items/org.png" className="max-w-[90%]"/>}
+            img1={<img src={bottles} className="max-w-[90%]"/>}
+            img2={<img src={rack} className="max-w-[90%]"/>}
+            img3={<img src={jars} className="max-w-[90%]"/>}
+            img4={<img src={kettle} className="max-w-[90%]"/>}
+            img5={<img src={chopper1} className="max-w-[90%]"/>}
+            img6={<img src={airfryer} className="max-w-[90%]"/>}
+            img7={<img src={blender} className="max-w-[90%]"/>}
+            img8={<img src={machine} className="max-w-[90%]"/>}
+            img9={<img src={mixer} className="max-w-[90%]"/>}
+            img10={<img src={chopper} className="max-w-[90%]"/>}
+            img11={<img src={processor} className="max-w-[90%]"/>}
+            img12={<img src={org} className="max-w-[90%]"/>}
         />
     </>
     )
